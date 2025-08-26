50a750082788921e6dbcd882ed76bfeff47b9456
ronesc <130864295+ronesc@users.noreply.github.com>
2025-08-26 15:08:44 -0300

    alterando 4

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ df = pd.read_csv('Spotify Youtube Dataset.csv')
 st.header('Spotify e YouTube Analise de Dados')
 
 #Agora, vamos criar o conteúdo do aplicativo baseado em Streamlit.
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('Spotify Youtube Dataset.csv')
+
+st.header('Spotify and YouTube Data Analysis Dashboard')
 
 st.write('Selecione as visualizações que você gostaria de ver:')
 
 build_histogram = st.checkbox('Criar um histograma de contagem de Streams')
-build_scatter = st.checkbox('Criar um gráfico de dispersão de Danceability vs Energy')
+build_scatter = st.checkbox('Gráfico de Dispersão de Views vs Stream: Para ver se há uma correlação entre a popularidade no YouTube (Views) e no Spotify (Stream)')
 
 if build_histogram:
     st.write('Criando um histograma para a contagem de Streams')
@@ -19,6 +26,6 @@ if build_histogram:
     st.plotly_chart(fig_hist, use_container_width=True)
 
 if build_scatter:
-    st.write('Criando um gráfico de dispersão de Danceability vs Energy')
-    fig_scatter = px.scatter(df, x="Danceability", y="Energy")
+    st.write('Criando um gráfico de dispersão de Views vs Stream')
+    fig_scatter = px.scatter(df, x="Views", y="Stream")
     st.plotly_chart(fig_scatter, use_container_width=True)
